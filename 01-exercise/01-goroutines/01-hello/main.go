b3140d59dbdfcdd129289128bc13b004cf0a0889
siwonpawel <siwonpawel@gmail.com>
2020-12-24 19:57:14 +0100

    ex 1

--- 01-exercise/01-goroutines/01-hello/main.go
+++ 01-exercise/01-goroutines/01-hello/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -12,19 +13,33 @@ func fun(s string) {
 	}
 }
 
+func goRoutine(s string, wg *sync.WaitGroup) {
+	wg.Add(1)
+	fun(s)
+	wg.Done()
+}
+
 func main() {
 	// Direct call
 	fun("direct call")
 
 	// TODO: write goroutine with different variants for function call.
 
+	wg := sync.WaitGroup{}
 	// goroutine function call
+	go goRoutine("go-routine by function call", &wg)
 
 	// goroutine with anonymous function
+	go func() {
+		goRoutine("go-routine with anonymous function", &wg)
+	}()
 
 	// goroutine with function value call
+	fn := goRoutine
+	go fn("go-routine by function value call", &wg)
 
 	// wait for goroutines to end
 
+	wg.Wait()
 	fmt.Println("done..")
 }
