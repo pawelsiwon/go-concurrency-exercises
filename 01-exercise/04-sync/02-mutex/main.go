cd19a38d0ce92151979fa6e00e6a1a26562b3684
siwonpawel <siwonpawel@gmail.com>
2020-12-25 15:26:21 +0100

    concurrent read

--- 01-exercise/04-sync/02-mutex/main.go
+++ 01-exercise/04-sync/02-mutex/main.go
@@ -12,7 +12,7 @@ func main() {
 
 	var balance int
 	var wg sync.WaitGroup
-	var mu sync.Mutex
+	var mu sync.RWMutex
 
 	deposit := func(amount int) {
 		mu.Lock()
@@ -20,11 +20,25 @@ func main() {
 		mu.Unlock()
 	}
 
+	read := func() int {
+		mu.RLock()
+		defer mu.RUnlock()
+		return balance
+	}
+
 	wg.Add(10)
 	for i := 0; i < 10; i++ {
 		go func() {
 			defer wg.Done()
 			deposit(1)
+		}()
+	}
+
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			fmt.Println(read())
 		}()
 	}
 
