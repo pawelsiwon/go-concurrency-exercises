43ce3e84f1063b074c18a1b0de60835256ad89e5
siwonpawel <siwonpawel@gmail.com>
2020-12-25 18:18:41 +0100

    termination of goroutines

--- 02-exercise/02-pipeline/03-pipeline/main.go
+++ 02-exercise/02-pipeline/03-pipeline/main.go
@@ -5,41 +5,57 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
+	"time"
 )
 
-func generator(nums ...int) <-chan int {
+func generator(done <-chan struct{}, nums ...int) <-chan int {
 	out := make(chan int)
 
 	go func() {
+		defer close(out)
 		for _, n := range nums {
-			out <- n
+			select {
+			case out <- n:
+			case <-done:
+				return
+			}
 		}
-		close(out)
 	}()
+
 	return out
 }
 
-func square(in <-chan int) <-chan int {
+func square(done <-chan struct{}, in <-chan int) <-chan int {
 	out := make(chan int)
 	go func() {
+		defer close(out)
 		for n := range in {
-			out <- n * n
+			select {
+			case out <- n * n:
+			case <-done:
+				return
+			}
 		}
-		close(out)
 	}()
+
 	return out
 }
 
-func merge(cs ...<-chan int) <-chan int {
+func merge(done <-chan struct{}, cs ...<-chan int) <-chan int {
 	out := make(chan int)
 	var wg sync.WaitGroup
 
 	output := func(c <-chan int) {
+		defer wg.Done()
 		for n := range c {
-			out <- n
+			select {
+			case out <- n:
+			case <-done:
+				return
+			}
 		}
-		wg.Done()
 	}
 
 	wg.Add(len(cs))
@@ -51,18 +67,25 @@ func merge(cs ...<-chan int) <-chan int {
 		wg.Wait()
 		close(out)
 	}()
+
 	return out
 }
 
 func main() {
-	in := generator(2, 3)
+	done := make(chan struct{})
 
-	c1 := square(in)
-	c2 := square(in)
+	in := generator(done, 2, 3)
 
-	out := merge(c1, c2)
+	c1 := square(done, in)
+	c2 := square(done, in)
+
+	out := merge(done, c1, c2)
 
 	// TODO: cancel goroutines after receiving one value.
 
 	fmt.Println(<-out)
+	close(done)
+
+	time.Sleep(10 * time.Millisecond)
+	println("Working goroutines:", runtime.NumGoroutine())
 }
