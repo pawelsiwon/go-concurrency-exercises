8ea219e9a1b6f7d93953f66dec9914e66bc3cfe6
siwonpawel <siwonpawel@gmail.com>
2020-12-25 14:01:53 +0100

    ex channels

--- 01-exercise/02-channel/02-channel/main.go
+++ 01-exercise/02-channel/02-channel/main.go
@@ -1,12 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	ch := make(chan int, 6)
+
 	go func() {
+		defer close(ch)
 		for i := 0; i < 6; i++ {
 			// TODO: send iterator over channel
+			ch <- i + 10
 		}
 	}()
 
 	// TODO: range over channel to recv values
+	for c := range ch {
+		fmt.Println("Recivied: ", c)
+	}
 
 }
