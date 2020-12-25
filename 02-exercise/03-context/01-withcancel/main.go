8b53cba6b00a2f27d23fc46b57649ca90dd07391
siwonpawel <siwonpawel@gmail.com>
2020-12-25 23:19:52 +0100

    context with cancel

--- 02-exercise/03-context/01-withcancel/main.go
+++ 02-exercise/03-context/01-withcancel/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"context"
+	"fmt"
+)
+
 func main() {
 
 	// TODO: generator -  generates integers in a separate goroutine and
@@ -8,10 +13,34 @@ func main() {
 	// they consume 5th integer value
 	// so that internal goroutine
 	// started by gen is not leaked.
-	generator := func() <-chan int {
+	generator := func(ctx context.Context) <-chan int {
+		c := make(chan int)
 
+		n := 1
+		go func() {
+			defer close(c)
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case c <- n:
+				}
+				n++
+			}
+		}()
+
+		return c
 	}
 
 	// Create a context that is cancellable.
+	ctx, cancel := context.WithCancel(context.Background())
 
+	ints := generator(ctx)
+
+	for n := range ints {
+		fmt.Println(n)
+		if n == 5 {
+			cancel()
+		}
+	}
 }
