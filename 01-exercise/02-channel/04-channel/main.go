d8b27dd3ac6a9633c81a16e711b771e754986540
siwonpawel <siwonpawel@gmail.com>
2020-12-25 14:08:56 +0100

    ex channels

--- 01-exercise/02-channel/04-channel/main.go
+++ 01-exercise/02-channel/04-channel/main.go
@@ -1,20 +1,33 @@
 package main
 
+import "fmt"
+
 // TODO: Implement relaying of message with Channel Direction
 
-func genMsg() {
+func genMsg(ch chan<- string) {
+	defer close(ch)
 	// send message on ch1
+	ch <- "sent from genMsg"
 }
 
-func relayMsg() {
+func relayMsg(ch1 <-chan string, ch2 chan<- string) {
+	defer close(ch2)
 	// recv message on ch1
+	text := <-ch1
 	// send it on ch2
+	ch2 <- text
 }
 
 func main() {
 	// create ch1 and ch2
 
-	// spine goroutine genMsg and relayMsg
+	ch1 := make(chan string)
+	ch2 := make(chan string)
 
+	// spine goroutine genMsg and relayMsg
+	go genMsg(ch1)
 	// recv message on ch2
+	go relayMsg(ch1, ch2)
+
+	fmt.Println("Message:", <-ch2)
 }
